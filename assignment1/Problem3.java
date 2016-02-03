686df393c5f1a2d24b1bd0f1f35596ca62f0eede
shershin <michael.shershin@gmail.com>
2016-02-03 15:38:50 -0500

    first commit and setting up work enviroment for assignment1

---
+++ assignment1/Problem3.java
@@ -0,0 +1,39 @@
+
+public class Problem3 {
+	
+	public static boolean checksum(int[] A, int t)
+	{
+		/*
+Input: an array, A, of n integers (positive, negative, or 0), and an integer value t.
+Output: “yes” if there exist three indices i, j and k, with 0 = i < j < k = n-1, such
+that A[i] + A[j] + A[k] = t, and “no” otherwise.
+		 */
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Test your checksum method
+		
+		int[] A = {28, -70, -23, 92, 56, -33, -77};
+		int t = -47;
+		System.out.println("There exist three indices i, j and k, such that A[i] + A[j] + A[k] =" + t + " : " + checksum(A,t));
+		
+		// Required: Please test your method with a different array and t, you can make up an array, and t
+		
+		// Complete the code here.
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ assignment1/Problem4.java
@@ -0,0 +1,46 @@
+
+public class Problem4 {
+
+	public static void partition(int[] A)
+	{
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int[] A = {4,3,9,2,7,6,5};
+		
+		System.out.println("Before partition:");
+		for(int i = 0; i < A.length; i++)
+		{
+			System.out.print(A[i] + " ");
+			
+		}
+		
+		partition(A);
+		
+		System.out.println("After partition:");
+		
+		System.out.println("Before partition:");
+		for(int i = 0; i < A.length; i++)
+		{
+			System.out.print(A[i] + " ");
+			
+		}
+		
+		
+		
+	}
+
+}
