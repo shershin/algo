bcd7489912fdd67d9dec3ca50c13276c9b7b5206
shershin <michael.shershin@gmail.com>
2016-02-08 16:41:46 -0500

    starting to code

--- assignment1/Problem3.java
+++ assignment1/Problem3.java
@@ -8,13 +8,23 @@ Input: an array, A, of n integers (positive, negative, or 0), and an integer val
 Output: yes if there exist three indices i, j and k, with 0 = i < j < k = n-1, such
 that A[i] + A[j] + A[k] = t, and no otherwise.
 		 */
-
-
-
-
-
-		 return false;
-
+		 /*/
+		 take in the arry
+		 get the length of said arry
+		 then create a sum of all items to see if it matches random int
+		 init all the things
+		 for loop
+		 then if statement
+		 /*/
+		 int sum = 0;
+		 for (int i = 0; i < A.length; i++){
+			 sum += A[i];
+		 }
+		 if (sum == t){
+			 return true;
+		 } else {
+			 return false;
+		 }
 	}
 
 	public static void main(String[] args) {
@@ -24,7 +34,7 @@ that A[i] + A[j] + A[k] = t, and no otherwise.
 
 		int[] A = {28, -70, -23, 92, 56, -33, -77};
 		int t = -47;
-		System.out.println("There exist three indices i, j and k, such that A[i] + A[j] + A[k] =" + t + " : " + checksum(A,t));
+		System.out.println("There exist three indices i, j and k, such that A[i] + A[j] + A[k] = " + t + " : " + checksum(A,t));
 
 		// Required: Please test your method with a different array and t, you can make up an array, and t
 
