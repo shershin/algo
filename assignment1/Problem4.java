6154e56bbcbcad8e66e3a5152bbb3984f3a848e5
shershin <michael.shershin@gmail.com>
2016-02-15 12:04:42 -0500

    got problem4 completly working

--- assignment1/Problem4.java
+++ assignment1/Problem4.java
@@ -7,22 +7,30 @@ public class Problem4 {
 		//check each element of the arry agaisnt the first and put the smaller ones before it
 		//covert to an alist
 		//convert it back
-		System.out.println(A.toString());
-		int[] arry = A;
+		//start with the basics
+		//a new array to hold the values
+		//pointers to everything else
+		int[] arry = new int[A.length];
 		int hold = A[0];
 		int start = 0;
 		int end = A.length - 1;
 		for (int i = 1; i < A.length; i++){
-			System.out.println(arry[i]);
-			if (arry[i] < hold){
-				A[start] = arry[i];
+			//if item is smaller than first element
+			//put it in front of the first element
+			//and move the first element back one
+			if (A[i] < hold){
+				arry[start] = A[i];
 				start++;
-				A[start] = hold;
+				arry[start] = hold;
+			//although if it is larger
+			//put that element at the end
 			} else {
-				A[end] = arry[i];
+				arry[end] = A[i];
 				end--;
 			}
 		}
+		//do some fancy smancy array coping tech
+		System.arraycopy(arry, 0, A, 0, arry.length);
 	}
 
 
