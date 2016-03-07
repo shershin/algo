3e62d4f9d5e9f5075335f5d986eb71337654df1b
shershin <michael.shershin@gmail.com>
2016-03-07 14:46:53 -0500

    looking at getting started on assignment2

---
+++ assignment2/Problem3.java
@@ -0,0 +1,29 @@
+
+public class Problem3 {
+
+	// Create a mergeklists() to merge 3 sorted arrays into one sorted array
+	// Input: 3 sorted arrays
+	// Output: one sorted array that contains all the elements from input arrays
+	// Complete the code here.
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] l1 = {1,5,9,10,20};
+		
+		int[] l2 = {2,4,5,6,7,9,15};
+		
+		int[] l3 = {3,8,13,15,22};
+		
+		// Required: Test your mergeklists() over l1, l2, l3,
+		// Complete the code here.
+		
+		
+		
+		
+	}
+
+}
---
+++ assignment2/Problem6.java
@@ -0,0 +1,27 @@
+
+public class Problem6 {
+
+	public static double f(int n )
+	{
+		// f(n) = 1  if n = 0
+		// f(n) = 1 + 1 / f(n-1)  if n > 0
+		// Complete the code here to make a recursive method to calculate f(n)
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n1 = 1, n2 = 10;
+		
+		// Required: Test your f() when n = 1, and when n = 10,
+		// Complete the code here.
+		
+		
+		
+		
+	}
+
+}
